1c520ea0e2326f989e8e62fa29b439f55c1d18c5
nhngcobo <nhngcobo@github.com>
2026-04-23 22:54:47 +0200

    Change Vite output to build/ to match Azure expectations

--- dsgrdms.client/vite.config.js
+++ dsgrdms.client/vite.config.js
@@ -49,6 +49,9 @@ export default defineConfig({
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
     },
+    build: {
+        outDir: 'build'
+    },
     server: isCI ? {} : {
         proxy: {
             '^/api': {
